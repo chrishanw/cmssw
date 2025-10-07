6cc617b998ab25ca6b97475261ed4e42b5372c20
Christian Wessel <christian.wessel@desy.de>
2025-10-07 17:43:02 +0200

    Add a new SoA for all information belonging to one region, as it seems to be best to parallelise over the regions by launching one kernel for each region. This could result in memory overhead as hits can be part of several regions, as well as several doublets / triplets per region, and the individual hit information are stored. If necessary, can be optimised later.

---
+++ DataFormats/EgammaReco/interface/PixelSeedingRegionSoA.h
@@ -0,0 +1,53 @@
+#ifndef DataFormats_EgammaReco_interface_PixelSeedingRegionSoA_h
+#define DataFormats_EgammaReco_interface_PixelSeedingRegionSoA_h
+
+#include <Eigen/Core>
+#include <cstdint>
+#include "DataFormats/SoATemplate/interface/SoACommon.h"
+#include "DataFormats/SoATemplate/interface/SoALayout.h"
+
+
+namespace reco {
+
+	using Vector3d = Eigen::Matrix<double, 3, 1>;
+	using Matrix4d = Eigen::Matrix<double, 4, 4>;
+
+	GENERATE_SOA_LAYOUT(PixelSeedingRegionLayout,
+        SOA_SCALAR(bool, isTriplet),
+        SOA_SCALAR(uint32_t, size),
+		SOA_SCALAR(float, ptMin),
+		SOA_SCALAR(float, vertexRBound),
+		SOA_SCALAR(float, vertexZBound),
+		SOA_SCALAR(float, tanLambdaRangeMin),
+		SOA_SCALAR(float, tanLambdaRangeMax),
+        SOA_SCALAR(float, phiMarginLeft),
+		SOA_SCALAR(float, phiMarginRight),
+		// SOA_EIGEN_SCALAR(Vector3d, direction),
+		// SOA_EIGEN_SCALAR(Vector3d, vertexPos),
+		SOA_SCALAR(float, directionX),
+		SOA_SCALAR(float, directionY),
+		SOA_SCALAR(float, directionZ),
+		SOA_SCALAR(float, vertexPosX),
+		SOA_SCALAR(float, vertexPosY),
+		SOA_SCALAR(float, vertexPosZ),
+		SOA_COLUMN(uint32_t, hit0Detid),
+		SOA_EIGEN_COLUMN(Vector3d, hit0LocalPos),
+		SOA_EIGEN_COLUMN(Vector3d, hit0LocalPosError),		// xx, xy, yy, see DataFormats/GeometrySurface/interface/LocalError.h
+		SOA_EIGEN_COLUMN(Vector3d, hit0GlobalPos),
+		SOA_EIGEN_COLUMN(Matrix4d, hit0GlobalPosError),		// see DataFormats/GeometryCommonDetAlgo/interface/GlobalErrorBase.h
+		SOA_COLUMN(uint32_t, hit1Detid),
+		SOA_EIGEN_COLUMN(Vector3d, hit1LocalPos),
+		SOA_EIGEN_COLUMN(Vector3d, hit1LocalPosError),
+		SOA_EIGEN_COLUMN(Vector3d, hit1GlobalPos),
+		SOA_EIGEN_COLUMN(Matrix4d, hit1GlobalPosError),
+		SOA_COLUMN(uint32_t, hit2Detid),
+		SOA_EIGEN_COLUMN(Vector3d, hit2LocalPos),
+		SOA_EIGEN_COLUMN(Vector3d, hit2LocalPosError),
+		SOA_EIGEN_COLUMN(Vector3d, hit2GlobalPos),
+		SOA_EIGEN_COLUMN(Matrix4d, hit2GlobalPosError),
+
+	)
+	using PixelSeedingRegionSoA = PixelSeedingRegionLayout<>;
+}  // namespace reco
+
+#endif
