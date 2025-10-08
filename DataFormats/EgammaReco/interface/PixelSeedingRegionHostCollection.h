16213f6e7b4276ed6e703cc08d5a1e723dde98e1
Christian Wessel <christian.wessel@desy.de>
2025-10-08 15:05:00 +0200

    Remove the first two SoAs that I decided not to use in favour of the new PixelSeedingRegionSoA.

--- DataFormats/EgammaReco/interface/PixelSeedingRegionHostCollection.h
+++ DataFormats/EgammaReco/interface/PixelSeedingRegionHostCollection.h
@@ -3,15 +3,11 @@
 
 #include "DataFormats/Portable/interface/PortableHostCollection.h"
 #include "DataFormats/EgammaReco/interface/PixelSeedingRegionSoA.h"
-#include "DataFormats/EgammaReco/interface/PixelSeedingRegionsSoA.h"
-#include "DataFormats/EgammaReco/interface/PixelSeedingTrackingHitsSoA.h"
 
 namespace reco {
 
   // Egamma pixel seeding SoAs in host memory
   using PixelSeedingRegionHostCollection = PortableHostCollection<PixelSeedingRegionSoA>;
-  using PixelSeedingRegionsHostCollection = PortableHostCollection<PixelSeedingRegionsSoA>;
-  using PixelSeedingTrackingHitsSoAHostCollection = PortableHostCollection<PixelSeedingTrackingHitsSoA>;
 }  // namespace reco
 
 #endif
