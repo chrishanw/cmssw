cde7738da739f7eae162321d1596c6117df58020
Christian Wessel <christian.wessel@desy.de>
2025-10-21 16:11:52 +0200

    Fill the globalpos error matrix in the SoA.

--- RecoTracker/TkSeedGenerator/plugins/alpaka/SeedCreatorFromRegionHitsEDProducerAlpaka.cc
+++ RecoTracker/TkSeedGenerator/plugins/alpaka/SeedCreatorFromRegionHitsEDProducerAlpaka.cc
@@ -122,40 +122,46 @@ namespace ALPAKA_ACCELERATOR_NAMESPACE {
 
       uint i = 0; 
       for (const SeedingHitSet& hits : regionSeedingHitSets) {
-        SeedingHitSet::ConstRecHitPointer tth0 = hits[0];
-        SeedingHitSet::ConstRecHitPointer tth1 = hits[1];
 
+        const auto toEigen = [](const auto& mat) {
+          using Matrix4d = Eigen::Matrix<double, 4, 4>;
+          const Matrix4d out{{mat.cxx(), mat.cyx(), mat.czx(), mat.ctx()},
+                             {mat.cyx(), mat.cyy(), mat.czy(), mat.cty()},
+                             {mat.czx(), mat.czy(), mat.czz(), mat.ctz()},
+                             {mat.ctx(), mat.cty(), mat.ctz(), mat.ctt()}};
+          return out;
+        };
+
+        SeedingHitSet::ConstRecHitPointer tth0 = hits[0];
         const auto& locPos0 = tth0->localPosition();
         const auto& locPosErr0 = tth0->localPositionError();
         const auto& globPos0 = tth0->globalPosition();
-        const auto& globPosErr0 = tth0->globalPositionError();
         psrhc.view()[i].hit0Detid() = tth0->rawId();
         psrhc.view()[i].hit0LocalPos() = Eigen::Vector3d(locPos0.x(), locPos0.y(), locPos0.z());
         psrhc.view()[i].hit0LocalPosError() = Eigen::Vector3d(locPosErr0.xx(), locPosErr0.xy(), locPosErr0.yy());
         psrhc.view()[i].hit0GlobalPos() = Eigen::Vector3d(globPos0.x(), globPos0.y(), globPos0.z());
-        // psrhc.view()[i].hit0GlobalPosError() = tth0->globalPositionError();
+         psrhc.view()[i].hit0GlobalPosError() = toEigen(tth0->globalPositionError());
 
+        SeedingHitSet::ConstRecHitPointer tth1 = hits[1];
         const auto& locPos1 = tth1->localPosition();
         const auto& locPosErr1 = tth1->localPositionError();
         const auto& globPos1 = tth1->globalPosition();
-        const auto& globPosErr1 = tth1->globalPositionError();
         psrhc.view()[i].hit1Detid() = tth1->rawId();
         psrhc.view()[i].hit1LocalPos() = Eigen::Vector3d(locPos1.x(), locPos1.y(), locPos1.z());
         psrhc.view()[i].hit1LocalPosError() = Eigen::Vector3d(locPosErr1.xx(), locPosErr1.xy(), locPosErr1.yy());
         psrhc.view()[i].hit1GlobalPos() = Eigen::Vector3d(globPos1.x(), globPos1.y(), globPos1.z());
-        // psrhc.view()[i].hit1GlobalPosError() = tth1->globalPositionError();
+        psrhc.view()[i].hit1GlobalPosError() = toEigen(tth1->globalPositionError());
 
         if (isTriplet) {
           SeedingHitSet::ConstRecHitPointer tth2 = hits[2];
           const auto& locPos2 = tth2->localPosition();
           const auto& locPosErr2 = tth2->localPositionError();
           const auto& globPos2 = tth2->globalPosition();
-          const auto& globPosErr2 = tth2->globalPositionError();
           psrhc.view()[i].hit2Detid() = tth2->rawId();
           psrhc.view()[i].hit2LocalPos() = Eigen::Vector3d(locPos2.x(), locPos2.y(), locPos2.z());
           psrhc.view()[i].hit2LocalPosError() = Eigen::Vector3d(locPosErr2.xx(), locPosErr2.xy(), locPosErr2.yy());
           psrhc.view()[i].hit2GlobalPos() = Eigen::Vector3d(globPos2.x(), globPos2.y(), globPos2.z());
-          // psrhc.view()[i].hit2GlobalPosError() = tth2->globalPositionError();
+          psrhc.view()[i].hit2GlobalPosError() = toEigen(tth2->globalPositionError());
         } else {
           psrhc.view()[i].hit2Detid() = 0;
           psrhc.view()[i].hit2LocalPos().setZero();
